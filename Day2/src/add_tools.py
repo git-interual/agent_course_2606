9ceaf8e277bcba74e3128d4209b02ce8c51deb85
Kiyong Kim <scgyong@gmail.com>
2026-06-17 11:36:41 +0900

    Day2 현재 진행 상황 기록

--- Day2/src/add_tools.py
+++ Day2/src/add_tools.py
@@ -6,8 +6,18 @@ import asyncio
 
 from agent_framework.openai import OpenAIChatClient
 from openai import AsyncAzureOpenAI
+from pydantic import Field
+from typing import Annotated
 
 load_dotenv()
+
+def get_weather(location: Annotated[str, Field(
+        description="도시 이름. 서울, 도쿄, 파리 등"
+    )]
+) -> str:
+    conditions = ["맛있어", "흐림", "비", "눈"]
+    print(f"Tool get_weather called with location: {location}")
+    return f"{location}의 오늘 날씨는 {conditions[0]}입니다."
 
 async def main() -> None:
     print("Hello, Agent!")
@@ -16,14 +26,6 @@ async def main() -> None:
     api_key = os.getenv("AZURE_OPENAI_API_KEY")
     model = os.getenv("AZURE_OPENAI_CHAT_MODEL")
     api_version = os.getenv("AZURE_OPENAI_API_VERSION")
-
-    class Param: pass
-    o = Param()
-    o.endpoint = endpoint
-    o.api_key = api_key
-    o.model = model
-    o.api_version = api_version
-    print(o.__dict__)
 
     azure_client = AsyncAzureOpenAI(
         azure_endpoint=endpoint,
@@ -37,10 +39,11 @@ async def main() -> None:
     ).as_agent(
         name="HelloAgent",
         instructions="너는 친절한 AI 비서야. 사용자의 질문에 친절하게 답변해줘.",
+        tools=get_weather,
     )
     print(f"{agent.name} is ready.")
 
-    result = await agent.run("프랑스 수도는 어디야?")
+    result = await agent.run("프랑스 파리 날씨는 어때?")
     print(result)
 
 if __name__ == "__main__":
