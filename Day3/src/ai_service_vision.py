1fd7e7ac3cef907412c27ee63f9549401cd45e76
KKY@MBP16 <scgyong@tukorea.ac.kr>
2026-06-18 17:09:26 +0900

    import 추가.

--- Day3/src/ai_service_vision.py
+++ Day3/src/ai_service_vision.py
@@ -1,2 +1,8 @@
 # image 분석
 # pip install azure-ai-vision-imageanalysis
+import os
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
+from PIL import Image, ImageDraw, ImageFont
